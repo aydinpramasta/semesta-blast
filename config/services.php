a0bde39b3a18d64cddf2f0cce3a07b6fe82c975e
Aydin Pramasta <aydin.pram@gmail.com>
2024-06-05 08:50:13 +0700

    feat: add sevima notification service credentials

--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         ],
     ],
 
+    'sevima-notification-service' => [
+        'url' => env('SEVIMA_NOTIFICATION_URL'),
+        'app_id' => env('SEVIMA_NOTIFICATION_APP_ID'),
+        'app_secret' => env('SEVIMA_NOTIFICATION_APP_SECRET'),
+    ],
 ];
